e66b84534b3dd5377e78b2326b6ce3397bfcd5f8
leo.jaimes <leo777jaimes@gmail.com>
2022-10-07 20:12:40 -0500

    upload

--- base-concert-venue/cypress/e2e/routes.cy.ts
+++ base-concert-venue/cypress/e2e/routes.cy.ts
@@ -6,3 +6,11 @@ it("displays correct heading when navigating to shows route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+
+it("displays correct heading when navigating to bands rout", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
